bd7e2c45f2aa462cde003542b102dab3f74e792a
khunanyajinamun <khunanyaji20118@gmail.com>
2024-12-06 10:07:04 +0700

    Add files via upload

---
+++ backends/controller/employee/employee.go
@@ -0,0 +1,35 @@
+package employee
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GET
+func GetEmployee(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Employee GET Method!",
+	})
+}
+
+// POST
+func PostEmployee(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Employee POST Method!",
+	})
+}
+
+// PUT
+func PutEmployee(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Employee PUT Method!",
+	})
+}
+
+// DELETE
+func DeleteEmployee(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Employee DELETE Method!",
+	})
+}
